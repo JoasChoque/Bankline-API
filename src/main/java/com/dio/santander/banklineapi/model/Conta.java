4599ff5332e31229934b517c539efbbde626feea
Joas Choque <61289634+JoasChoque@users.noreply.github.com>
2022-05-03 17:09:46 -0300

    Creating Class Model API

--- src/main/java/com/dio/santander/banklineapi/model/Conta.java
+++ src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -1,5 +1,22 @@
 package com.dio.santander.banklineapi.model;
 
 public class Conta {
-
+	private Long num;
+	private Double salario;
+	
+	
+	public Long getNum() {
+		return num;
+	}
+	public void setNum(Long num) {
+		this.num = num;
+	}
+	public Double getSalario() {
+		return salario;
+	}
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	
+	
 }
